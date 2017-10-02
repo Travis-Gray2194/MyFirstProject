a969e2c8b25d6305763716dfa167647271568bbd
TravisGray1 <travisogray1@gmail.com>
2017-10-02 17:41:58 -0400

    git add .

---
+++ src/Program4Myself.java
@@ -0,0 +1,12 @@
+public class Program4Myself {
+        public static void main(String args[])
+        {
+            //Basic application with name and age
+            System.out.println("Travis Gray");
+            System.out.println("Age:25");
+
+
+        }
+
+    }
+
